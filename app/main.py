4d359af4d41d52af25f4916eee07190402eb5f37
sidsigma3 <sidsigma3@gmail.com>
2026-02-04 16:47:24 +0530

    changes

--- app/main.py
+++ app/main.py
@@ -4,14 +4,18 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="HRMS Lite API")
 
+origins = [
+    "http://localhost:3000", 
+    "https://hrms-ui-xi.vercel.app",  
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 app.include_router(employee_routes.router)
 app.include_router(attendance_routes.router)
 
